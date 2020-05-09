580dfe34a2c95cfbfe71e74c16f88ae685db9042
Joey Lee <jayd005@gmail.com>
2020-05-10 01:54:43 +1000

    Better sprite loading configuration

--- breakout/assets/repository.go
+++ breakout/assets/repository.go
@@ -1,7 +1,6 @@
 package assets
 
 import (
-	"fmt"
 	"games50-go/breakout/assets/fonts"
 	"games50-go/breakout/assets/graphics"
 	"games50-go/breakout/assets/sounds"
@@ -44,76 +43,95 @@ func LoadAssets() {
 	loadSprites()
 }
 
-// TODO should manage this better
 func loadSprites() {
 	sprites = make(map[string]map[string]*ebiten.Image)
 
 	arrowsSpriteSheet := assets.LoadImage(graphics.Arrows_png)
-	sprites["arrows"] = make(map[string]*ebiten.Image)
-	sprites["arrows"]["left"] = arrowsSpriteSheet.SubImage(image.Rect(0, 0, 24, 24)).(*ebiten.Image)
-	sprites["arrows"]["right"] = arrowsSpriteSheet.SubImage(image.Rect(24, 0, 48, 24)).(*ebiten.Image)
+	arrowSprites := assets.CutSpritesheet(arrowsSpriteSheet, assets.CutSpritesheetConfig{
+		XEnd: 48,
+		YEnd: 24,
+		Sprites: []assets.SpriteConfig{{
+			Width:  24,
+			Height: 24,
+		}},
+	})
+	assets.GroupSprites(arrowSprites, sprites, []string{"arrows"}, []string{"left", "right"})
 
 	breakoutSpriteSheet := assets.LoadImage(graphics.Breakout_png)
-	paddleColours := []string{"blue", "green", "red", "purple"}
-	yOffset := 64
-	for _, colour := range paddleColours {
-		// paddle height = 16 // width 32, 64, 96, 128
-		spriteGroup := fmt.Sprintf("paddles-%s", colour)
-		sprites[spriteGroup] = make(map[string]*ebiten.Image)
-
-		sprites[spriteGroup]["smallest"] = breakoutSpriteSheet.SubImage(image.Rect(0, yOffset, 32, yOffset+16)).(*ebiten.Image)
-		sprites[spriteGroup]["small"] = breakoutSpriteSheet.SubImage(image.Rect(32, yOffset, 96, yOffset+16)).(*ebiten.Image)
-		sprites[spriteGroup]["large"] = breakoutSpriteSheet.SubImage(image.Rect(96, yOffset, 192, yOffset+16)).(*ebiten.Image)
-		yOffset += 16
-		sprites[spriteGroup]["largest"] = breakoutSpriteSheet.SubImage(image.Rect(0, yOffset, 128, yOffset+16)).(*ebiten.Image)
-		yOffset += 16
-	}
+	paddleColours := []string{"paddles-blue", "paddles-green", "paddles-red", "paddles-purple"}
+	paddleSprites := assets.CutSpritesheet(breakoutSpriteSheet, assets.CutSpritesheetConfig{
+		YOffset: 64,
+		XLimit:  128,
+		XEnd:    128,
+		YEnd:    192,
+		Sprites: []assets.SpriteConfig{{
+			Width:  32,
+			Height: constants.PaddleHeight,
+		}, {
+			Width:  64,
+			Height: constants.PaddleHeight,
+		}, {
+			Width:  96,
+			Height: constants.PaddleHeight,
+		}, {
+			Width:  128,
+			Height: constants.PaddleHeight,
+		}},
+	})
+	assets.GroupSprites(paddleSprites, sprites, paddleColours, []string{"smallest", "small", "large", "largest"})
 
 	ballColours := []string{"blue", "green", "red", "purple", "yellow", "grey", "gold"}
-	// x-offset 96 y-offset 48 x-finish 128
-	sprites["balls"] = make(map[string]*ebiten.Image)
-	xStart := 96
-	xOffset := 96
-	xLimit := 128
-	yOffset = 48
-	for _, colour := range ballColours {
-		sprites["balls"][colour] = breakoutSpriteSheet.SubImage(image.Rect(xOffset, yOffset, xOffset+constants.BallWidth, yOffset+constants.BallHeight)).(*ebiten.Image)
-		xOffset += constants.BallWidth
-		if xOffset == xLimit {
-			yOffset += constants.BallHeight
-			xOffset = xStart
-		}
-	}
+	ballSprites := assets.CutSpritesheet(breakoutSpriteSheet, assets.CutSpritesheetConfig{
+		XOffset: 96,
+		YOffset: 48,
+		XLimit:  128,
+		XEnd:    120,
+		YEnd:    64,
+		Sprites: []assets.SpriteConfig{{
+			Width:  constants.BallWidth,
+			Height: constants.BallHeight,
+		}},
+	})
+	assets.GroupSprites(ballSprites, sprites, []string{"balls"}, ballColours)
 
 	brickTiers := []string{"basic", "extra", "super", "ultra"}
-	brickColours := []string{"blue", "green", "red", "purple", "yellow"}
-	xStart = 0
-	xOffset = 0
-	xLimit = 192
-	yOffset = 0
-	for _, colour := range brickColours {
-		spriteGroup := fmt.Sprintf("bricks-%s", colour)
-		sprites[spriteGroup] = make(map[string]*ebiten.Image)
+	brickColours := []string{"bricks-blue", "bricks-green", "bricks-red", "bricks-purple", "bricks-yellow"}
+	brickSprites := assets.CutSpritesheet(breakoutSpriteSheet, assets.CutSpritesheetConfig{
+		XEnd: 64,
+		YEnd: 64,
+		Sprites: []assets.SpriteConfig{{
+			Width:  constants.BrickWidth,
+			Height: constants.BrickHeight,
+		}},
+	})
+	assets.GroupSprites(brickSprites, sprites, brickColours, brickTiers)
 
-		for _, tier := range brickTiers {
-			sprites[spriteGroup][tier] = breakoutSpriteSheet.SubImage(image.Rect(xOffset, yOffset, xOffset+constants.BrickWidth, yOffset+constants.BrickHeight)).(*ebiten.Image)
-			xOffset += constants.BrickWidth
-			if xOffset == xLimit {
-				yOffset += constants.BrickHeight
-				xOffset = xStart
-			}
-		}
-	}
 	sprites["bricks"] = make(map[string]*ebiten.Image)
 	sprites["bricks"]["locked"] = breakoutSpriteSheet.SubImage(image.Rect(160, 48, 160+constants.BrickWidth, 48+constants.BrickHeight)).(*ebiten.Image)
 
-	sprites["hearts"] = make(map[string]*ebiten.Image)
-	sprites["hearts"]["full"] = breakoutSpriteSheet.SubImage(image.Rect(128, 48, 128+constants.HeartWidth, 48+constants.HeartHeight)).(*ebiten.Image)
-	sprites["hearts"]["empty"] = breakoutSpriteSheet.SubImage(image.Rect(138, 48, 138+constants.HeartWidth, 48+constants.HeartHeight)).(*ebiten.Image)
+	heartSprites := assets.CutSpritesheet(breakoutSpriteSheet, assets.CutSpritesheetConfig{
+		XOffset: 128,
+		YOffset: 48,
+		XEnd:    148,
+		YEnd:    58,
+		Sprites: []assets.SpriteConfig{{
+			Width:  constants.HeartWidth,
+			Height: constants.HeartHeight,
+		}},
+	})
+	assets.GroupSprites(heartSprites, sprites, []string{"hearts"}, []string{"full", "empty"})
 
-	sprites["powerups"] = make(map[string]*ebiten.Image)
-	sprites["powerups"]["extra-balls"] = breakoutSpriteSheet.SubImage(image.Rect(128, 192, 128+constants.PowerupWidth, 192+constants.PowerupHeight)).(*ebiten.Image)
-	sprites["powerups"]["key"] = breakoutSpriteSheet.SubImage(image.Rect(144, 192, 144+constants.PowerupWidth, 192+constants.PowerupHeight)).(*ebiten.Image)
+	powerupSprites := assets.CutSpritesheet(breakoutSpriteSheet, assets.CutSpritesheetConfig{
+		XOffset: 128,
+		YOffset: 192,
+		XEnd:    160,
+		YEnd:    208,
+		Sprites: []assets.SpriteConfig{{
+			Width:  constants.PowerupWidth,
+			Height: constants.PowerupHeight,
+		}},
+	})
+	assets.GroupSprites(powerupSprites, sprites, []string{"powerups"}, []string{"extra-balls", "key"})
 }
 
 func GetFont(name string) font.Face {
--- internal/assets/image.go
+++ internal/assets/image.go
@@ -8,6 +8,20 @@ import (
 	"github.com/hajimehoshi/ebiten"
 )
 
+type SpriteConfig struct {
+	Width  int
+	Height int
+}
+
+type CutSpritesheetConfig struct {
+	XOffset int
+	YOffset int
+	XLimit  int
+	XEnd    int
+	YEnd    int
+	Sprites []SpriteConfig
+}
+
 func LoadImage(imageData []byte) *ebiten.Image {
 	decoded, _, err := image.Decode(bytes.NewReader(imageData))
 	if err != nil {
@@ -16,4 +30,52 @@ func LoadImage(imageData []byte) *ebiten.Image {
 
 	loadedImage, _ := ebiten.NewImageFromImage(decoded, ebiten.FilterDefault)
 	return loadedImage
+}
+
+func CutSpritesheet(sheet *ebiten.Image, cutConfig CutSpritesheetConfig) []*ebiten.Image {
+	sheetWidth, _ := sheet.Size()
+
+	var cutSprites []*ebiten.Image
+
+	xOffset := cutConfig.XOffset
+	yOffset := cutConfig.YOffset
+	spriteConfigIndex := 0
+
+	for {
+		sprite := cutConfig.Sprites[spriteConfigIndex]
+
+		cut := sheet.SubImage(image.Rect(xOffset, yOffset, xOffset+sprite.Width, yOffset+sprite.Height)).(*ebiten.Image)
+		cutSprites = append(cutSprites, cut)
+
+		xOffset += sprite.Width
+		if xOffset == cutConfig.XEnd && yOffset+sprite.Height == cutConfig.YEnd {
+			break
+		}
+
+		if xOffset == sheetWidth || xOffset == cutConfig.XLimit {
+			// break to new row in sprite sheet
+			yOffset += sprite.Height
+			xOffset = cutConfig.XOffset
+		}
+
+		// loop sprite configs
+		spriteConfigIndex = (spriteConfigIndex + 1) % len(cutConfig.Sprites)
+	}
+
+	return cutSprites
+}
+
+// Groups the sprite array into different groups and names them
+// Updates the groupedSprites map in place
+func GroupSprites(sprites []*ebiten.Image, groupedSprites map[string]map[string]*ebiten.Image, groups []string, names []string) {
+	spriteIndex := 0
+	for _, group := range groups {
+		groupedSprites[group] = make(map[string]*ebiten.Image)
+
+		for _, name := range names {
+			groupedSprites[group][name] = sprites[spriteIndex]
+
+			spriteIndex++
+		}
+	}
 }
