497074170520831e3b61cb1b4aa527fede2959d5
Joey Lee <jayd005@gmail.com>
2020-05-11 18:29:35 +1000

    Add some comments to the particle emitter configs

--- internal/particles/config.go
+++ internal/particles/config.go
@@ -5,13 +5,19 @@ import (
 	"time"
 )
 
+// ParticleEmitterConfig - These configs define how the particle emitter should behave
 type ParticleEmitterConfig struct {
+	// The max number of particles that will be active at any one time
 	MaxParticles int
-	Lifetime     Range
+	// The lifetime range of a particle (how long it should stay on the screen)
+	Lifetime Range
+	// The X, Y acceleration of a particle - affects the velocity
 	Acceleration Acceleration
 	Spawn        Spawn
-	Colours      []color.Color
-	EmitterLife  time.Duration
+	// The colours to apply to the particle (interpolated depending on the particle lifetime)
+	Colours []color.Color
+	// How long the emitter should keep emitting for. If this is empty, the emitter will stop after max particles has been reached
+	EmitterLife time.Duration
 }
 
 type Range struct {
